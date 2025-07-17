d308abaf5d08c8973eb7d148c56beb1687d169d0
thethsuqualy <thethsukhin.qualymm@gmail.com>
2025-07-17 11:35:00 +0630

    post change

--- page-blog.php
+++ page-blog.php
@@ -30,7 +30,7 @@ get_header();
                     $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
                     $args = array(
                         'post_type'      => 'post',
-                        'posts_per_page' => 5,
+                        'posts_per_page' => 6,
                         'paged'          => $paged,
                     );
 
